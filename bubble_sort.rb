234c30334604683d8f645cc2172428baf760cb13
dangerstephen <dangersteve1@gmail.com>
2016-12-13 10:21:33 -0800

    added another solution, so it can be seen doing do diffrent ways

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -14,3 +14,24 @@ def bubble_sort(arr)
 
 arr
 end
+
+# OR you can do it this way
+def bubble_sort(array)
+      swapped = true
+      length = array.length-1
+      while swapped do
+          i = 0
+          swapped = false
+          while i <length
+              n = i +1
+              if array[i] > array[n]
+                  temp = array[i]
+                  array[i]= array[n]
+                  array[n] = temp
+                  swapped = true
+              end
+             i+=1
+          end
+      end
+      return array
+end
